c989ea1ed6ff04bc6da46a33b4c7a7b0fe7170ef
epifanio <epiesasha@me.com>
2016-09-07 20:33:12 -0400

    Sync image browser tools

--- imagedisplay.py
+++ imagedisplay.py
@@ -21,18 +21,23 @@ import os
 
 class ImageDisplay(QObject):
     def init(self):
-        self.imagepath = '/Users/epi/Desktop/rawdisplay/imgs/'
+        self.imagepath = '/media/epi/backup1/imgs/'
         self.imagelist = pickle.load( open('imagelist.p', 'rb'))
         self.imageindex = 1
         self.ZoomStepValue=50
         self.w = GuiWidget()
+
+        self.w.ImageIndexspinBox.setMaximum(len(self.imagelist))
+        self.w.ImageIndexSlider.setMaximum(len(self.imagelist))
 
         self.w.fwd.clicked.connect(self.increaseimageindex)
         self.w.fwd.clicked.connect(self.addImage)
 
         self.w.rwd.clicked.connect(self.decreaseimageindex)
         self.w.rwd.clicked.connect(self.addImage)
-                
+
+
+        # ZOOM
         self.w.ZoomStepSlider.valueChanged.connect(self.setValueZoomStepspinBox)
         
         self.w.ZoomStepSlider.valueChanged.connect(self.printFunction)
@@ -44,38 +49,66 @@ class ImageDisplay(QObject):
         self.w.ZoomStepSlider.valueChanged.connect(self.setValue)
         
         self.w.ZoomStepspinBox.valueChanged.connect(self.setValue)
-        
-        self.w.actionExit.triggered.connect(self.quitAll)
 
+        # Image Index
+        self.w.ImageIndexSlider.valueChanged.connect(self.setValueImageIndexspinBox)
+
+        self.w.ImageIndexSlider.valueChanged.connect(self.printFunction)
+
+        self.w.ImageIndexspinBox.valueChanged.connect(self.setValueImageIndexSlider)
+
+        self.w.ImageStepspinBox.valueChanged.connect(self.setImageIndexStepValue)
+
+        # Exit
+        self.w.actionExit.triggered.connect(self.quitAll)
 
 
         self.w.show()
 
+
+
+
     def getimage(self):
         return self.imagelist[self.imageindex]
 
+
+    def setImageIndexStepValue(self):
+        self.w.ImageIndexspinBox.setSingleStep(self.w.ImageStepspinBox.value())
+        self.w.ImageIndexSlider.setSingleStep(self.w.ImageStepspinBox.value())
+
+    def setimageindex(self):
+        self.w.ImageIndexSlider.value()
+
     def decreaseimageindex(self):
-        self.imageindex=self.imageindex-1
+        self.imageindex=self.imageindex-self.w.ImageStepspinBox.value()
 
     def increaseimageindex(self):
         print(self.w.ImageStepspinBox.value())
         self.imageindex=self.imageindex+self.w.ImageStepspinBox.value()
 
 
+    def setValueImageIndexspinBox(self, z):
+        self.imageindex = int(z)
+        self.w.ImageIndexspinBox.setSingleStep(self.w.ImageStepspinBox.value())
+        self.w.ImageIndexspinBox.setValue(self.imageindex)
 
+    def setValueImageIndexSlider(self, z):
+        self.imageindex = int(z)
+        self.w.ImageIndexSlider.setSingleStep(self.w.ImageStepspinBox.value())
+        self.w.ImageIndexSlider.setValue(self.imageindex)
 
 
     def setValueZoomStepspinBox(self, z):
-        self.ZoomStepValue = float(z)
-        self.w.ZoomStepspinBox.setRange(1, 100)
+        self.ZoomStepValue = int(z)
+        #self.w.ZoomStepspinBox.setRange(1, 100)
         self.w.ZoomStepspinBox.setSingleStep(1)
         self.w.ZoomStepspinBox.setValue(self.ZoomStepValue)
-    
+
     
     def setValueZoomStepSlider(self, z):
         self.ZoomStepValue = int(z)
         self.w.ZoomStepSlider.setMinimum(1)
-        self.w.ZoomStepSlider.setMaximum(100)
+        #self.w.ZoomStepSlider.setMaximum(100)
         self.w.ZoomStepSlider.setValue(self.ZoomStepValue)
 
     def setValue(self, v):
@@ -86,7 +119,7 @@ class ImageDisplay(QObject):
         print(self.ZoomStepValue)
 
     def addImage(self):
-        print(self.imagelist[self.imageindex])
+        print(os.path.join(self.imagepath,self.imagelist[self.imageindex]))
         self.pixmap = QPixmap(os.path.join(self.imagepath,self.imagelist[self.imageindex]))
         width=self.pixmap.width()*(self.ZoomStepValue/100.)
         height=self.pixmap.height()*(self.ZoomStepValue/100.)
